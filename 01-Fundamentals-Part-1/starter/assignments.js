2e143b0bf5193dcd0f766d102cf57d42cf50309a
pascalmorgenthaler <morgenthalerpascal@gmail.com>
2023-12-27 16:12:30 +0100

    a

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,18 @@
+/* const country = 'Switzerland';
+const continent = 'Europe';
+let population = '8 Million';
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+const isIsland = false;
+const language = 'Schwiizerdütsch';
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language); */
+
+
+
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,91 @@
+/*
 let js = 'amazing';
-console.log(40 + 8 + 23 - 10)
+console.log(40 + 8 + 23 - 10);
 
-console.log("Jonas")
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+
+// Variable Name Conventions 
+let jonas_matilda = "JM";
+let $funtion = 27;
+
+let person = 'jonas';
+let PI = 3.1415;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'Programmer';
+let job2 = 'Teacher';
+
+console.log(myFirstJob);
+*/
+
+/* let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1999;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null); */
+
+/* let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+// const job;
+
+var job = 'programmer';
+job = 'teacher'
+
+lastName = 'Schmedtmann';
+console.log(lastName); */
+
+// Math Operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2019;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 which is equal to 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+// Assignment Operators
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x /= 4; // x = x / 4 = 25
+x++; // x = x + 1 = 26
+x--; // x = x - 1 = 25
+x--; // x = x - 1 = 24
+console.log(x);
+
+// Comparison Operators
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18)
