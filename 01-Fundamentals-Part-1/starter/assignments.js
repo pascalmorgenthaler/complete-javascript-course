cde3745894d7a1a32f6c852f97019f363aafae54
pascalmorgenthaler <morgenthalerpascal@gmail.com>
2023-12-28 12:36:46 +0100

    a

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,4 +1,4 @@
-/* const country = 'Switzerland';
+const country = 'Switzerland';
 const continent = 'Europe';
 let populationSwitzerland = 8; // Millions
 
@@ -27,5 +27,56 @@ console.log(populationSwitzerland > populationAverage);
 const description1 = country + ' is in ' + continent + ', and its ' + populationSwitzerland + ' Million people speak ' + language + '.';
 console.log(description1);
 
+const description2 = `${country} is in ${continent}, and its ${populationSwitzerland} Million people speak ${language}.`;
+console.log(description2);
 
- */
+console.log('9' - '5');
+console.log('19' - '13' + '17');
+console.log('19' - '13' + 17);
+console.log('123' < 57);
+console.log(5 + 6 + '4' + 9 - 4 - 2);
+
+// const numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+
+// if (numNeighbours === 1) {
+//     console.log('Only 1 border!');
+// } else if (numNeighbours > 1) {
+//     console.log('More than 1 border!');
+// } else {
+//     console.log('No borders.')
+// }
+
+if (language === 'english' && populationSwitzerland < 50 && !isIsland) {
+    console.log(`You should live in ${country}!`);
+} else {
+    console.log(`${country} does not meet your criteria :(`);
+}
+
+switch (language) {
+    case 'chinese':
+    case 'mandarin':
+        console.log('MOST number of native speakers!');
+        break;
+    case 'spanish':
+        console.log('2nd place in number of native speakers');
+        break;
+    case 'english':
+        console.log('3rd place');
+        break;
+    case 'hindi':
+        console.log('Number 4');
+        break;
+    case 'arabic':
+        console.log('5th most spoken language');
+        break;
+    default:
+        console.log('Great Language too :D');
+}
+
+const greaterThan33 = populationSwitzerland >= 33 ? "Switzerland's population is above average." : "Switzerland's population is below average."
+
+console.log(greaterThan33);
+
+console.log(
+    `${country}'s population is ${populationSwitzerland > 33 ? 'above' : 'below'} average.`
+)
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -124,7 +124,7 @@ const markHigherBMI = BMIMark > BMIJohn;
 
 console.log(markHigherBMI); */
 
-const firstName = 'Jonas';
+/* const firstName = 'Jonas';
 const job = 'teacher';
 const birthYear = 1991;
 const year = 2037;
@@ -147,4 +147,232 @@ line');
 console.log(`String
 with
 mulitple
-lines`);
+lines`); */
+
+/* const age = 15;
+
+if (age >= 18) {
+    console.log('Sarah can start driving license 🤓')
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years :(`)
+}
+
+const birthYear = 1999;
+let century;
+
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+
+console.log(century); */
+
+
+// Coding Challenge 2 ------------------------------------------------------------
+
+/* const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+console.log(BMIMark, BMIJohn);
+
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(markHigherBMI);
+
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI (${BMIMark}) is higher than John's!`)
+} else {
+    console.log(`John's BMI (${BMIJohn}) is higher than Marks's!`)
+} */
+
+// Type conversion
+/* const inputYear = '1991';
+console.log(Number(inputYear), inputYear)
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+console.log('I am ' + 23 + ' years old') // + operator converts the number to a string -> automatic type coercion
+console.log('23' - '10' - 3); // is 10 in the console -> - operator converts a string to a number
+console.log('23' + '10' + 3); // is 23103 in the console -> + operator converts a number to a string
+console.log('23' * '2'); // is 46 in the console -> * and / also change strings to numbers
+
+let n = '1' + 1; // string 11
+n = n - 1; // string 11 is convertet to the number 11 and 1 is subtracted
+console.log(n); */
+
+// 5 falsy values: 0, '', undefined, nulll, NaN
+
+/* console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 100;
+if (money) {
+    console.log("Don't spend it all. ;)");
+} else {
+    console.log('You should get a job!'); // this one is executed -> why? money is set to number 0
+}
+
+let height = 0; // this creates a bug, since 0 is also a falsy value and it will get us height is undefined -> we can fix this using logical operators
+if (height) {
+    console.log('Yay! Height is defined');
+} else {
+    console.log('Height is UNDEFINED');
+} */
+/*
+const age = '18';
+if (age === 18) console.log('You just became an adult :D (strict)');
+
+if (age == 18) console.log('You just became an adult :D (loose)');
+
+const favouriteNumber = Number(prompt("What's your favourite number?"));
+console.log(favouriteNumber);
+console.log(typeof favouriteNumber);
+ */
+/* if (favouriteNumber == 8) { // '23' == 23
+    console.log('Cool! 8 is an amazing number!');
+}
+
+/* if (favouriteNumber === 8) { // does not work without the number function in the prompt funtion
+    console.log('Cool! 8 is an amazing number!');
+} else if (favouriteNumber === 7) {
+    console.log('7 is also a cool number');
+} else if (favouriteNumber === 9) {
+    console.log('9 is also a cool number');
+} else {
+    console.log('Number is not 8 or 7 or 9')
+}
+
+if(favouriteNumber !== 8) console.log('Why not 8?'); */
+
+/* const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision); // and operatoer
+console.log(hasDriversLicense || hasGoodVision); // or operator
+console.log(!hasDriversLicense); // not operator
+
+if (hasDriversLicense && hasGoodVision) {
+    console.log('Sarah is able to drive!');
+} else {
+    console.log('Someone else should drive...')
+}
+
+const isTired = false; // C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+    console.log('Sarah is able to drive!');
+} else {
+    console.log('Someone else should drive...')
+} */
+
+// Coding Challenge 3 ------------------------------------------------------------
+
+// const averageDolphins = (96 + 108 + 89) / 3;
+// const averageKoalas = (88 + 91 + 110) / 3;
+
+// const averageDolphins = (97 + 112 + 101) / 3;
+// const averageKoalas = (109 + 95 + 123) / 3;
+
+// const averageDolphins = (80 + 112 + 101) / 3;
+// const averageKoalas = (92 + 95 + 106) / 3;
+
+// if (averageDolphins > averageKoalas && averageDolphins >= 100) {
+//     console.log('YAY, Dolphins win!')
+// } else if (averageDolphins < averageKoalas && averageKoalas >= 100) {
+//     console.log('Nooo, Koalas win.')
+// } else if (averageDolphins === averageKoalas && averageDolphins >= 100) {
+//     console.log('Draw.')
+// } else {
+//     console.log('No one wins.')
+// }
+
+// const day = 'thursday';
+
+// switch (day) {
+//     case 'monday': // day === 'monday'
+//         console.log('Plan course structure');
+//         console.log('Go to coding meetup');
+//         break;
+//     case 'tuesday':
+//         console.log('Prepare theory videos');
+//         break;
+//     case 'wednesday':
+//     case 'thursday':
+//         console.log('Write code examples');
+//         break;
+//     case 'friday':
+//         console.log('Record videos');
+//         break;
+//     case 'saturday':
+//     case 'sunday':
+//         console.log('Enjoy the weekend.');
+//         break;
+//     default:
+//         console.log('Not a valid day!');
+// }
+
+// if (day === 'monday') {
+//     console.log('Plan course structure');
+//     console.log('Go to coding meetup');
+// } else if (day === 'tuesday') {
+//     console.log('Prepare theory videos');
+// } else if (day === 'wednesday' || day === 'thursday') {
+//     console.log('Write code examples')
+// } else if (day === 'friday') {
+//     console.log('Record videos');
+// } else if (day === 'saturday' || day === 'sunday') {
+//     cconsole.log('Enjoy the weekend.')
+// } else {
+//     console.log('Not a valid day!')
+// }
+
+// 3 + 4 // expression, it does produce a value
+// 1991 // expression, it is a value in JavaScript
+// true && false && !false // expression, it produces a value
+
+// // this is a statement, it does not produce a value, it simply declares something and performs some actions
+// if (23 > 10) {
+//     const str = '23 is bigger'; // the string itself is an expression, the whole line of code is a statement
+// }
+
+// // the conditional operator -> basically like writing an if else statement in one line
+// const age = 19;
+// age >= 18 ? console.log('I like to drink wine 🍷') : console.log('I like to drink water 💧');
+
+// const drink = age >= 18 ? 'wine 🍷' : 'water 💧';
+// console.log(drink);
+
+// let drink2;
+// if (age >= 18) {
+//     drink2 = ' wine 🍷';
+// } else {
+//     drink2 = 'water 💧';
+// }
+// console.log(drink2);
+
+// console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💧'}`)
+
+
+// Coding Challenge 4 ------------------------------------------------------------
+
+// const bill = 40
+
+// const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}.`)
